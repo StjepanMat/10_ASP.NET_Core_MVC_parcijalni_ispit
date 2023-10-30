16a90cc9931bb5bec4d12546dbeeb74c71c39fab
StjepanMat <stjepan.mateljan@gmail.com>
2023-10-30 15:09:00 +0100

    TaskController, TodoController added.

---
+++ Ispit.Todo/Controllers/TodoListController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Ispit.Todo.Data;
+using Ispit.Todo.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Ispit.Todo.Controllers
+{
+    //[Authorize(Roles ="User")]
+    public class TodoListController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TodoListController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: TodoList
+        public async Task<IActionResult> Index()
+        {
+              return _context.TodoLists != null ? 
+                          View(await _context.TodoLists.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.TodoLists'  is null.");
+        }
+
+        // GET: TodoList/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.TodoLists == null)
+            {
+                return NotFound();
+            }
+
+            var todoList = await _context.TodoLists
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (todoList == null)
+            {
+                return NotFound();
+            }
+
+            return View(todoList);
+        }
+
+        // GET: TodoList/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TodoList/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Description,UserId")] TodoList todoList)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(todoList);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(todoList);
+        }
+
+        // GET: TodoList/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.TodoLists == null)
+            {
+                return NotFound();
+            }
+
+            var todoList = await _context.TodoLists.FindAsync(id);
+            if (todoList == null)
+            {
+                return NotFound();
+            }
+            return View(todoList);
+        }
+
+        // POST: TodoList/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,UserId")] TodoList todoList)
+        {
+            if (id != todoList.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(todoList);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TodoListExists(todoList.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(todoList);
+        }
+
+        // GET: TodoList/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.TodoLists == null)
+            {
+                return NotFound();
+            }
+
+            var todoList = await _context.TodoLists
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (todoList == null)
+            {
+                return NotFound();
+            }
+
+            return View(todoList);
+        }
+
+        // POST: TodoList/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.TodoLists == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.TodoLists'  is null.");
+            }
+            var todoList = await _context.TodoLists.FindAsync(id);
+            if (todoList != null)
+            {
+                _context.TodoLists.Remove(todoList);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TodoListExists(int id)
+        {
+          return (_context.TodoLists?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
